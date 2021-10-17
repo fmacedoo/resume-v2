3a3feb0697daa6f7bc33df3513ce0084df54ff96
Filipe Azevêdo <filipeazevedo@Filipes-iMac-Pro.local>
2021-10-17 12:49:12 -0300

    added resume fetch 'n render

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "resume-v2",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "",
-        spaceId: "",
-      },
+    siteMetadata: {
+        siteUrl: 'https://fmacedoo.github.io/resume/',
+        title: 'resume-v2',
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-react-helmet",
-  ],
+    plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                accessToken: process.env.CONTENTFUL_ACCESSTOKEN,
+                spaceId: 'vb9n6v0rkykx',
+            },
+        },
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-react-helmet',
+    ],
 };
